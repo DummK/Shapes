fbf33f58aa836d60945ed2e2344f833340bfff3a
Filip <fpolski22@gmail.com>
2025-04-04 08:10:17 +0200

    koniec

--- src/test/java/shape/ShapeCollectorTestSuite.java
+++ src/test/java/shape/ShapeCollectorTestSuite.java
@@ -64,5 +64,103 @@ public class ShapeCollectorTestSuite {
                             //Then
                             assertEquals(result, circle);
                         }
-    }
+            }
+
+    @Nested
+        @DisplayName("Test Square")
+            class SquareTest {
+
+                @Test
+                    @DisplayName("Test Add Square to ArrayList")
+                        void testAddSquare() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Square square = new Square(10);
+
+                            //When
+                            shapeCollector.addFigure(square);
+
+                            //Then
+                            assertEquals(shapeCollector.getFigure(0), square);
+                        }
+
+                @Test
+                    @DisplayName("Test Remove Square from ArrayList")
+                        void testRemoveSquare() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Square square = new Square(10);
+                            shapeCollector.addFigure(square);
+
+                            //When
+                            boolean result = shapeCollector.removeFigure(square);
+
+                            //Then
+                            assertTrue(result);
+                        }
+
+                @Test
+                    @DisplayName("Test Get Square from ArrayList")
+                        void testGetSquare() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Square square = new Square(10);
+                            shapeCollector.addFigure(square);
+
+                            //When
+                            Shape result = shapeCollector.getFigure(0);
+
+                            //Then
+                            assertEquals(result, square);
+                        }
+            }
+
+    @Nested
+        @DisplayName("Test triangle")
+            class TriangleTest {
+
+                @Test
+                    @DisplayName("Test Add Triangle to ArrayList")
+                        void testAddTriangle() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Triangle triangle = new Triangle(10, 10, 10, 5);
+
+                            //When
+                            shapeCollector.addFigure(triangle);
+
+                            //Then
+                            assertEquals(shapeCollector.getFigure(0), triangle);
+                        }
+
+                @Test
+                    @DisplayName("Test Remove Square from ArrayList")
+                        void testRemoveSquare() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Triangle triangle = new Triangle(10, 10, 10, 5);
+                            shapeCollector.addFigure(triangle);
+
+                            //When
+                            boolean result = shapeCollector.removeFigure(triangle);
+
+                            //Then
+                            assertTrue(result);
+                        }
+
+                @Test
+                    @DisplayName("Test Get Square from ArrayList")
+                        void testGetSquare() {
+                            //Given
+                            shapeCollector = new ShapeCollector();
+                            Square square = new Square(10);
+                            shapeCollector.addFigure(square);
+
+                            //When
+                            Shape result = shapeCollector.getFigure(0);
+
+                            //Then
+                            assertEquals(result, square);
+                        }
+            }
 }
